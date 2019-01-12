041b28856d9a23293b2ae3bfde5855f9d4bd4fca
ryansteed <rsteed11@gmail.com>
2019-01-12 16:46:45 -0500

    Updated regression with controls, also a non-time regression

--- scripts/r/regression.R
+++ scripts/r/regression.R
@@ -1,9 +1,41 @@
 setwd("/Users/Ryan/datamaster/data")
-dat = read.table("timedata.tsv", sep="\t", skip=1)
-colnames(dat) = c("t", "k")
+dat = read.table("colonial/FEATURES-TIME-DATA_12Jan19.tsv", sep="\t", skip=1)
 head(dat)
+colnames(dat) = c(
+  "k", 
+  "t", 
+  "t_0", 
+  "patent_num_claims", 
+  "patent_num_citedby_us", 
+  "patent_processing_time",
+  "inventor_id", 
+  "inventor_total_num_patents", 
+  "inventor_key_id", 
+  "assignee_type", 
+  "assignee_total_num_patents", 
+  "assignee_id", 
+  "assignee_key_id", 
+  "cpc_category", 
+  "cpc_group_id", 
+  "nber_category_id", 
+  "nber_subcat_id", 
+  "uspc_mainclass_id", 
+  "uspc_subclass_id", 
+  "ipc_class", 
+  "ipc_main_group", 
+  "wipo_field_id"
+)
+# process dates
+dat[,"t_0"] = as.Date(dat[,"t_0"])
+## center dates and convert to diff time in weeks since min
+dat[,"t_0"] = as.numeric(dat[,"t_0"] - min(dat[,"t_0"]), units="weeks")
+head(dat)
+
 # plot(dat['t'] + dat['t']^2, log(dat['k']))
-fit = lm(log(k) ~ t + t^2, data=dat)
+fit = lm(
+  log(k) ~ t + t_0 + patent_num_citedby_us + patent_processing_time + patent_num_claims, 
+  data=dat
+)
 summary(fit)
 # Other useful functions 
 # coefficients(fit) # model coefficients
@@ -14,16 +46,21 @@ summary(fit)
 # vcov(fit) # covariance matrix for model parameters 
 # influence(fit) # regression diagnostics
 
+dat_final = c(nrow(dat))
 k=0
 j=0
 for (i in 1:nrow(dat)) {
-  if (dat[i,1]==0) {
+  if (dat[i,"t"]==0) {
+    l=i-1
+    # log previous data point as index of a final result
+    dat_final = append(dat_final,l)
+    # run a regression on 
     if (i>1) {
-      fit = lm(log(k) ~ t, data=dat[c(k:i),])
+      fit = lm(log(k) ~ t, data=dat[c(k:l),])
       coeff = coefficients(fit)
       if (coeff['t']>.36) {
         print("Found max")
-        plot(dat[c(k:i),'t'], dat[c(k:i),'k'])
+        plot(dat[c(k:l),'t'], dat[c(k:l),'k'])
       }
       if(j==0) {
         coeffs = coeff
@@ -40,3 +77,13 @@ head(coeffs)
 summary(coeffs[,'t'])
 hist(coeffs[,'t'])
 max(coeffs[,'t'])
+
+
+# viewing the patents without a time dimension - using only final knowledge impact score
+dat2 = dat[dat_final,]
+summary(dat2[,"t_0"])
+fit = lm(
+  log(k) ~ t + t_0 + patent_num_citedby_us,
+  data=dat2
+)
+summary(fit)
