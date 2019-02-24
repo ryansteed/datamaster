e1d1c7f3d03ee62129a6cd010e8c512fa16aba4d
ryansteed <rsteed11@gmail.com>
2019-02-23 20:46:36 -0500

    :arrow_upper_right: Generalized descriptive stats to any test file

--- scripts/r/descriptive_stats.R
+++ scripts/r/descriptive_stats.R
@@ -2,7 +2,13 @@ setwd("/Users/Ryan/datamaster/data")
 library(stargazer)
 library(ggplot2)
 library(cowplot)
-dat = read.csv("colonial/METRICS_both_None_coherent-light_19Feb19.csv", sep=",", header=TRUE)
+
+# Input
+date = "20Feb19"
+query = "transportation"
+
+dat = read.csv(sprintf("colonial/METRICS_both_None_%s_%s.csv", query, date), sep=",", header=TRUE)
+
 print = dat[,!(names(dat) == "node")]
 colnames(print) = c(
   "Forward Cites",
@@ -13,7 +19,8 @@ colnames(print) = c(
   "Knowledge (H Index)",
   "Knowledge (Forward Cites)"
 )
-sink("r/metrics_coherent-light.tex", append=FALSE, split=FALSE)
+summary(print)
+sink(sprintf("r/metrics_%s.tex", query), append=FALSE, split=FALSE)
 stargazer(print, summary.stat=c("mean", "sd", "min", "max"), title="")
 sink()
 
@@ -27,11 +34,11 @@ hp = ggplot(to_plot, aes(x=xx, fill=yy, coulour=yy)) +
   ggtitle("TKC Distribution By Weighting Method") +
   theme_classic() +
   theme(legend.title=element_blank()) +
-  theme(legend.position="top")
+  theme(legend.position="top") +
   ylab("Density") +
   xlab("Log(K)")
 hp
-ggsave("r/tkc_dist.png", hp)
+ggsave(sprintf("r/tkc_dist_%s.png", query), hp)
 
 # to_plot2 = data.frame(xx=c(dat$knowledge_h_index, dat$knowledge_forward_cites), yy=rep(c("H Index","Forward Cites"),each=length(log_h)))
 hp = ggplot(to_plot, aes(yy, xx)) + 
@@ -42,16 +49,4 @@ hp = ggplot(to_plot, aes(yy, xx)) +
   ylab("K") +
   xlab("Weighting Method")
 hp
-ggsave("r/tkc_boxplot.png", hp)
-
-# hp + facet_grid(. ~ yy)
-
-
-
-#to_plot = data.frame(log_h, log_f)
-#p1 = ggplot(to_plot, aes(x=log_f)) + stat_density(alpha=0.7) + theme_classic()
-#p2 = ggplot(to_plot, aes(x=log_h)) + stat_density(alpha=0.7) + theme_classic()
-
-#p = plot_grid(p1, p2)
-#p
-# save_plot("plot.pdf", p, ncol=2)
+ggsave(sprintf("r/tkc_boxplot_%s.png", query), hp)
