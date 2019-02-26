2a3f7daeac073e0762de735330029fcd3815c654
ryansteed <rsteed11@gmail.com>
2019-02-25 19:35:39 -0500

    :sparkles: Updated regression for new data format + factor parsing

--- scripts/r/regression.R
+++ scripts/r/regression.R
@@ -1,42 +1,10 @@
 setwd("/Users/Ryan/datamaster/data")
-dat = read.table("colonial/FEATURES-TIME-DATA_12Jan19.tsv", sep="\t", skip=1)
-head(dat)
-colnames(dat) = c(
-  "k",
-  "t",
-  "t_0",
-  "patent_num_claims", 
-  "patent_num_citedby_us",
-  "patent_processing_time",
-  "inventor_id", 
-  "inventor_total_num_patents", 
-  "inventor_key_id", 
-  "assignee_type", 
-  "assignee_total_num_patents", 
-  "assignee_id", 
-  "assignee_key_id", 
-  "cpc_category",
-  "cpc_group_id", 
-  "nber_category_id", 
-  "nber_subcat_id", 
-  "uspc_mainclass_id", 
-  "uspc_subclass_id", 
-  "ipc_class", 
-  "ipc_main_group", 
-  "wipo_field_id"
-)
+dat = read.table("colonial/FEATURES_both_None_coherent-light_25Feb19.csv", sep=",", header=TRUE)
 # process dates
-dat[,"t_0"] = as.Date(dat[,"t_0"])
+dat[,"patent_date"] = as.Date(dat[,"patent_date"])
 ## center dates and convert to diff time in weeks since min
-dat[,"t_0_center"] = as.numeric(dat[,"t_0"] - min(dat[,"t_0"]), units="weeks")
-head(dat)
+dat[,"patent_date_center"] = as.numeric(dat[,"patent_date"] - min(dat[,"patent_date"]), units="weeks")
 
-plot(dat[,'t'] + dat[,'t']^2 + dat[,'t']^3, log(dat[,'k']))
-fit = lm(
-  log(k) ~ t + t_0 + patent_num_citedby_us + patent_processing_time + patent_num_claims, 
-  data=dat
-)
-summary(fit)
 # Other useful functions 
 # coefficients(fit) # model coefficients
 # confint(fit, level=0.95) # CIs for model parameters 
@@ -46,60 +14,38 @@ summary(fit)
 # vcov(fit) # covariance matrix for model parameters 
 # influence(fit) # regression diagnostics
 
-dat_final = c(nrow(dat))
-k=0
-j=0
-for (i in 1:nrow(dat)) {
-  if (dat[i,"t"]==0) {
-    l=i-1
-    # log previous data point as index of a final result
-    dat_final = append(dat_final,l)
-    # run a regression on 
-    if (i>1) {
-      fit = lm(log(k) ~ t, data=dat[c(k:l),])
-      coeff = coefficients(fit)
-      if (coeff['t']>.36) {
-        print("Found max")
-        plot(dat[c(k:l),'t'], dat[c(k:l),'k'])
-      }
-      if(j==0) {
-        coeffs = coeff
-      }
-      else {
-        coeffs = rbind(coeffs, coeff) 
-      }
-      k=i
-      j=j+1 
-    }
-  }
+levels_to_list = function(vec, convertToInteger) {
+  strList = lapply(
+    vec, 
+    function(x) 
+      lapply(
+        strsplit(gsub("\\]", "", (gsub("\\[", "", as.character(x)))), " "), 
+        function(y) 
+          unlist(lapply(y[!y==""], function(z) if (convertToInteger) strtoi(z) else z))
+      )
+    )
+  return(strList)
 }
-head(coeffs)
-summary(coeffs[,'t'])
-hist(coeffs[,'t'])
-max(coeffs[,'t'])
 
-
+for (list_dat in c("inventor", "assignee")) {
+  total_num = paste(list_dat,"total_num_patents",sep="_")
+  dat[total_num] = levels_to_list(dat[total_num], TRUE)
+  dat[paste(list_dat,"max_num_patents", sep="_")] = sapply(dat[,total_num], function(x) max(unlist(x))) 
+}
 # viewing the patents without a time dimension - using only final knowledge impact score
-dat2 = dat[dat_final,]
-summary(dat2[,"t_0_center"])
 fit = lm(
-  log(k) ~ t + t_0_center + patent_num_citedby_us,
-  data=dat2
+  knowledge_forward_cites ~ patent_num_claims + patent_processing_time + inventor_max_num_patents + assignee_max_num_patents,
+  data=dat
 )
 summary(fit)
 
 # subsample for aia implementation
-dat2_before = dat2[dat2$t_0 > "2011-09-16",]
-dat2_after = dat2[dat2$t_0 < "2011-09-16",]
-summary(dat2_before[,"t_0_center"])
+dat_before = dat[dat$patent_date > "2011-09-16",]
+dat_after = dat2[dat$patent_date < "2011-09-16",]
+summary(dat_before[,"patent_date_center"])
 fit = lm(
-  log(k) ~ t + t_0_center + patent_num_citedby_us,
-  data=dat2_before
+  log(k) ~ patent_date + patent_num_citedby_us,
+  data=dat_before
 )
 summary(fit)
-summary(dat2_after[,"t_0_center"])
-fit = lm(
-  log(k) ~ t + t_0_center + patent_num_citedby_us,
-  data=dat2_after
-)
 summary(fit)
